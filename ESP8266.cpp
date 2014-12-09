432d6fe379977f8a4cb56a48f736ec0b364f08cc
RationalAsh <ashwinnarayan1994@gmail.com>
2014-12-09 19:41:35 +0530

    Did some initial work

---
+++ ESP8266.cpp
@@ -0,0 +1,29 @@
+#include "Arduino.h"
+#include "ESP8266.h"
+#include <SoftwareSerial.h>
+
+ESP8266::ESP8266(int RX=10, int TX=11)
+{
+    //Set up software serial on pins 10 and 11
+    //Set baud rate to 9600
+}
+
+void ESP8266::setMode(int MODE)
+{
+    //Send AT command to set the mode
+    //of the ESP8266
+}
+
+void ESP8266::setAccessPoint(String SSID, String password)
+{
+    //Set the parameters of the ESP8266
+    //in access point mode
+}
+
+void ESP8266::listAccessPoints()
+{
+    //Send AT command to list access points
+    //Get list of access points as a string
+    //Return string
+}
+
---
+++ ESP8266.h
@@ -0,0 +1,32 @@
+/* 
+   ESP8266.h - Library for the ESP8266 Wifi
+   module.
+
+   Created by Ashwin Narayan, December 9, 2014
+
+   Code is public domain
+*/
+
+
+#ifndef ESP8266_h
+#define ESP8266_h
+
+#include "Arduino.h"
+
+class ESP8266
+{
+public:
+    //RX goes to TX of module
+    //TX goes to RX of module
+    ESP8266(int RX=10, int TX=11);
+    void setMode(int MODE);
+    void setAccessPoint(String SSID, String password);
+    String listAccessPoints();
+
+private:
+    int mode;
+    int _RX;
+    int _TX;
+};
+
+#endif
